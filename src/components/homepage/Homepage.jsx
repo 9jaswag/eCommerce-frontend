597e0157a4011873add97cfea8c56c3a450a6452
Chuks Opia <chuks24ng@yahoo.co.uk>
2019-06-08 17:27:07 +0100

    feat: add pagination to homepage

--- src/components/homepage/Homepage.jsx
+++ src/components/homepage/Homepage.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import ReactPaginate from "react-paginate";
 import Header from "../shared/header/Header";
 import ProductCard from "../shared/product/ProductCard";
 import Sidebar from "../shared/sidebar/Sidebar";
@@ -10,15 +11,55 @@ export default () => {
     count: 0,
     rows: []
   });
+  const [paginationMetadata, setPaginationMetadata] = useState({
+    page: 1,
+    limit: 12
+  });
 
-  const fetchProducts = async () => {
-    const response = await fetch(`${baseURL}/products`)
-      .then(response => response.json())
-      .then(response => response);
+  useEffect(() => {
+    const buildProductURL = () => {
+      const { page, limit } = paginationMetadata;
+      return `${baseURL}/products?page=${page}&limit=${limit}`;
+    };
 
-    setProducts(response);
+    const fetchProducts = async () => {
+      const response = await fetch(buildProductURL())
+        .then(response => response.json())
+        .then(response => response);
+
+      setProducts(response);
+    };
+    fetchProducts();
+  }, [paginationMetadata]);
+
+  const handlePagination = page => {
+    setPaginationMetadata({
+      ...paginationMetadata,
+      page: parseInt(`${page.selected + 1}`, 10)
+    });
   };
-  fetchProducts();
+
+  const createPagination = () => {
+    const { count } = products;
+    const { limit } = paginationMetadata;
+
+    const totalPages = Math.ceil(count / limit);
+    return (
+      <ReactPaginate
+        previousLabel={<i className="pagination-previous">previous</i>}
+        nextLabel={<i className="pagination-next">next</i>}
+        breakLabel={<span>&hellip;</span>}
+        breakClassName={"pagination-ellipsis"}
+        pageCount={totalPages}
+        marginPagesDisplayed={1}
+        pageRangeDisplayed={3}
+        onPageChange={handlePagination}
+        containerClassName={"pagination-list"}
+        activeLinkClassName={"is-current"}
+        pageLinkClassName={"pagination-link"}
+      />
+    );
+  };
 
   return (
     <>
@@ -32,12 +73,21 @@ export default () => {
             </div>
             <div className={`column ${styles.set_min_width}`}>
               <div className="columns is-multiline">
-                {products.count === 0
-                  ? "Loading"
-                  : products.rows.map(product => (
-                      <ProductCard key={product.product_id} product={product} />
-                    ))}
+                {products.count === 0 ? (
+                  <i className="fas fa-spinner" />
+                ) : (
+                  products.rows.map(product => (
+                    <ProductCard key={product.product_id} product={product} />
+                  ))
+                )}
               </div>
+              <nav
+                className="pagination is-centered is-small is-rounded"
+                role="navigation"
+                aria-label="pagination"
+              >
+                {products.count > 0 && createPagination()}
+              </nav>
             </div>
           </div>
         </div>
