85084d9f96894a9f90f5ae65ec4fcb0a2e39143d
Chuks Opia <chuks24ng@yahoo.co.uk>
2019-06-16 17:23:53 +0100

    feat: add homepage banner

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import ReactPaginate from "react-paginate";
 import ProductCard from "../shared/product/ProductCard";
 import Sidebar from "../shared/sidebar/Sidebar";
 import styles from "./home.module.scss";
 import Loader from "../shared/loader/Loader";
+import banner1 from "../../assets/images/banner1.jpg";
+import banner2 from "../../assets/images/banner2.jpg";
+import banner3 from "../../assets/images/banner3.jpg";
 
 export default () => {
   const baseURL = "https://backendapi.turing.com";
@@ -79,6 +83,92 @@ export default () => {
               <Sidebar />
             </div>
             <div className={`column ${styles.set_min_width}`}>
+              <div className="">
+                <section className="hero">
+                  <div className="tile is-ancestor">
+                    <div className="tile is-vertical is-8">
+                      <div className="tile">
+                        <div className="tile is-parent is-vertical">
+                          <article className="tile is-child notification is-primary">
+                            <p className="title">Fashion for all places</p>
+                            <p className="subtitle">
+                              Fashion for every region.
+                            </p>
+                            <figure className="image is-4by3 mb-1">
+                              <img src={banner1} alt="first banner" />
+                            </figure>
+                            <Link
+                              to="/department/1"
+                              className="button is-warning is-uppercase"
+                            >
+                              Shop Now
+                            </Link>
+                          </article>
+                        </div>
+                        <div className="tile is-parent">
+                          <article className="tile is-child notification is-info">
+                            <p className="title">Amazing Fashion</p>
+                            <p className="subtitle">
+                              With a touch from nature.
+                            </p>
+                            <figure className="image is-4by3 mb-1">
+                              <img src={banner2} alt="second banner" />
+                            </figure>
+                            <Link
+                              to="/department/2"
+                              className="button is-warning is-uppercase"
+                            >
+                              Shop Now
+                            </Link>
+                          </article>
+                        </div>
+                      </div>
+                      <div className="tile is-parent">
+                        <article className="tile is-child notification is-danger">
+                          <p className="title">Fashion for all seasons</p>
+                          <p className="subtitle">Top designs and brands</p>
+                          <div className="content">
+                            <Link
+                              to="/department/1"
+                              className="button is-warning is-uppercase"
+                            >
+                              Shop Now
+                            </Link>
+                          </div>
+                        </article>
+                      </div>
+                    </div>
+                    <div className="tile is-parent">
+                      <article className="tile is-child notification is-success">
+                        <div className="content">
+                          <p className="title">Seasonal Fashion</p>
+                          <p className="subtitle">
+                            With even more swag for every season.
+                          </p>
+                          <div className="content">
+                            <div className="image is-4by5 mb-1">
+                              <img src={banner3} alt="third banner" />
+                            </div>
+                            <Link
+                              to="/department/3"
+                              className="button is-warning is-uppercase"
+                            >
+                              Shop Now
+                            </Link>
+                          </div>
+                        </div>
+                      </article>
+                    </div>
+                  </div>
+                </section>
+              </div>
+              <h3 className="is-uppercase is-size-4 has-text-centered my-3">
+                {`Showing ${
+                  paginationMetadata.page
+                } - ${paginationMetadata.page * 12} 0f ${
+                  products.count
+                } products`}
+              </h3>
               <div className="columns is-multiline">
                 {products.count === 0 ? (
                   <div className={styles.loader_wrapper}>
