2b8f5d6611617263bb1a2d3c0474c4acc0ed0dfd
Chuks Opia <chuks24ng@yahoo.co.uk>
2019-06-22 15:36:24 +0100

    Updates

---
+++ precache-manifest.8d3b97d09aa9622ad39c37355bab993e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adeae31336997ef6a64fa7fc3558226b",
+    "url": "/9jaswag/eCommerce-frontend/index.html"
+  },
+  {
+    "revision": "fd2a852a17607d6ba53c",
+    "url": "/9jaswag/eCommerce-frontend/static/css/2.4e96b846.chunk.css"
+  },
+  {
+    "revision": "2cc3ef22dbaeeb0bda01",
+    "url": "/9jaswag/eCommerce-frontend/static/css/main.58b8b818.chunk.css"
+  },
+  {
+    "revision": "fd2a852a17607d6ba53c",
+    "url": "/9jaswag/eCommerce-frontend/static/js/2.00d34675.chunk.js"
+  },
+  {
+    "revision": "2cc3ef22dbaeeb0bda01",
+    "url": "/9jaswag/eCommerce-frontend/static/js/main.16f17c97.chunk.js"
+  },
+  {
+    "revision": "a469d28c426e31d9c871",
+    "url": "/9jaswag/eCommerce-frontend/static/js/runtime~main.242c8cae.js"
+  },
+  {
+    "revision": "635c7a3bea7c49f8500058a0b1514aea",
+    "url": "/9jaswag/eCommerce-frontend/static/media/banner1.635c7a3b.jpg"
+  },
+  {
+    "revision": "0cba0448a9819c0bf8f9aceda2fb301f",
+    "url": "/9jaswag/eCommerce-frontend/static/media/banner2.0cba0448.jpg"
+  },
+  {
+    "revision": "464bcadc542fa949e3a40a9da40337e0",
+    "url": "/9jaswag/eCommerce-frontend/static/media/banner3.464bcadc.jpg"
+  },
+  {
+    "revision": "afa6a28d0ee0b5e7d55b7a5aecdfedec",
+    "url": "/9jaswag/eCommerce-frontend/static/media/empty-cart.afa6a28d.png"
+  },
+  {
+    "revision": "4fbd79ab8cbf058c8d5ee7b14e9c3a9b",
+    "url": "/9jaswag/eCommerce-frontend/static/media/stripe.4fbd79ab.png"
+  }
+]);
